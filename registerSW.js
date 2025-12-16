0e24dfd2bb374e2ad6b8a22c08ad88dca6eb08df
shivang <shivang>
2025-12-16 19:21:59 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/event/sw.js', { scope: '/event/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
